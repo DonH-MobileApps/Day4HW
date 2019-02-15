b22c6d2bd6cd8664b2202d5da9b41f6df13cd8d0
DonH-MobileApps <dharris.csu@yahoo.com>
2019-02-15 01:10:12 -0500

    1/2 finished

---
+++ app/src/main/java/com/example/day4hw/Activity2.java
@@ -0,0 +1,32 @@
+package com.example.day4hw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    TextView tvFirstName;
+    TextView tvLastName;
+    TextView tvAddress;
+    TextView tvCity;
+    TextView tvState;
+    TextView tvPostalCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+
+       /* tvFirstName = ;
+        tvLastName  =
+        tvAddress   =
+        tvCity      =
+        tvState     =
+        tvPostalCode =
+*/
+
+    }
+
+}
